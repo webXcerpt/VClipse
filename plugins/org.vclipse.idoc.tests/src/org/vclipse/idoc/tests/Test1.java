b45136ed6520168b3fc9962f06a9f5264959e43e
Tim Geisler <tg@webxcerpt.com>
2011-11-25 08:15:19 +0000

    skeleton for a test plugin for IDoc language

---
+++ plugins/org.vclipse.idoc.tests/src/org/vclipse/idoc/tests/Test1.java
@@ -0,0 +1,25 @@
+package org.vclipse.idoc.tests;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
+import org.eclipselabs.xtext.utils.unittesting.XtextTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.vclipse.idoc.IDocInjectorProvider;
+
+@RunWith(XtextRunner2.class)
+@InjectWith(IDocInjectorProvider.class)
+public class Test1 extends XtextTest {
+
+	public Test1() {
+		super("Test1");
+	}
+
+	@Test
+	public void test1() {
+		ignoreSerializationDifferences();
+		testFile("test1.idoc");
+	}
+
+}
