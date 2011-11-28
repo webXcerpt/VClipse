cd3ed83748963351f0da8eab380195c659af196d
AS <as@webxcerpt.com>
2011-11-28 12:12:48 +0000

    added keyword test

---
+++ plugins/org.vclipse.idoc.tests/src/org/vclipse/idoc/tests/KeywordTests.java
@@ -0,0 +1,27 @@
+package org.vclipse.idoc.tests;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
+import org.eclipselabs.xtext.utils.unittesting.XtextTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.vclipse.idoc.IDocInjectorProvider;
+
+@RunWith(XtextRunner2.class)
+@InjectWith(IDocInjectorProvider.class)
+public class KeywordTests extends XtextTest {
+
+	public KeywordTests() {
+		super(KeywordTests.class.getSimpleName());
+	}
+	
+	@Test
+	public void keywordsTests() {
+		testKeyword("idoc");
+		testKeyword("segment");
+		testKeyword("import");
+		testKeyword("{");
+		testKeyword("}");
+		testKeyword("=");
+	}
+}
