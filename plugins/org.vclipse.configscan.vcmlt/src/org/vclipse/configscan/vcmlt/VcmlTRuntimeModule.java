e833ce3dc839ba856ad03b47cfb63106808f22e1
Tim Geisler <tg@webxcerpt.com>
2012-01-11 21:36:20 +0000

    corrected name provider, global scope provider, and value converter configuration to be compatible with VCML

--- plugins/org.vclipse.configscan.vcmlt/src/org/vclipse/configscan/vcmlt/VcmlTRuntimeModule.java
+++ plugins/org.vclipse.configscan.vcmlt/src/org/vclipse/configscan/vcmlt/VcmlTRuntimeModule.java
@@ -3,9 +3,22 @@
  */
 package org.vclipse.configscan.vcmlt;
 
+import org.eclipse.xtext.conversion.IValueConverterService;
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
+import org.vclipse.base.NullQualifiedNameConverter;
+import org.vclipse.vcml.conversion.VCMLValueConverter;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class VcmlTRuntimeModule extends org.vclipse.configscan.vcmlt.AbstractVcmlTRuntimeModule {
 
+	@Override
+	public Class<? extends IValueConverterService> bindIValueConverterService() {
+		return VCMLValueConverter.class;
+	}
+	
+	public Class<? extends IQualifiedNameConverter> bindIQualifiedNameConverter() {
+		return NullQualifiedNameConverter.class;
+	}
 }
