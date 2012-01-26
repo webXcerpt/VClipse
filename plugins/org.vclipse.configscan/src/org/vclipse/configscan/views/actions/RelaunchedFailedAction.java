5b80f4282357475ac9bf710aabe99c0a92c8ac7a
AS <as@webxcerpt.com>
2012-01-26 09:12:53 +0000

    improvements after model change

--- plugins/org.vclipse.configscan/src/org/vclipse/configscan/views/actions/RelaunchedFailedAction.java
+++ plugins/org.vclipse.configscan/src/org/vclipse/configscan/views/actions/RelaunchedFailedAction.java
@@ -10,7 +10,8 @@ import org.vclipse.configscan.impl.TestCaseAdapterFactory;
 import org.vclipse.configscan.impl.TestCaseAdapterFactory.TestCaseAdapter;
 import org.vclipse.configscan.impl.model.TestCase;
 import org.vclipse.configscan.impl.model.TestCase.Status;
-import org.vclipse.configscan.utils.TestCaseFactory;
+import org.vclipse.configscan.impl.model.TestGroup;
+import org.vclipse.configscan.impl.model.TestRun;
 import org.vclipse.configscan.views.ConfigScanView;
 import org.vclipse.configscan.views.ConfigScanViewInput;
 
@@ -25,42 +26,34 @@ public class RelaunchedFailedAction extends SimpleTreeViewerAction {
 
 	@Override
 	public void run() {
-//		Object object = treeViewer.getInput();
-//		if(object instanceof ConfigScanViewInput) {
-//			ConfigScanViewInput input = (ConfigScanViewInput)object;
-//			for(TestCase testCase : input.getTestCases()) {
-//				Object adapter = testCase.getAdapter(TestRunAdapter.class);
-//				if(adapter != null) {
-//					TestRunAdapter testRunAdapter = (TestRunAdapter)adapter;
-//					testRunAdapter.setFilter(new FailureTestObjectFilter());
-//					EObject testModel = testRunAdapter.getTestModel();
-//					ResourceSet resourceSet = testModel.eResource().getResourceSet();
-//					handleChildren(testCase, resourceSet);
-//					testRunAdapter.getTestCase().clearChildren();
-//				}
-//			}
-//			treeViewer.setInput(input);
-//		}
+		Object object = treeViewer.getInput();
+		if(object instanceof ConfigScanViewInput) {
+			ConfigScanViewInput input = (ConfigScanViewInput)object;
+			for(TestRun testRun : input.getTestRuns()) {
+				testRun.setFilter(new FailureTestObjectFilter());
+				EObject testModel = testRun.getTestModel();
+				handleChildren(testRun, testModel.eResource().getResourceSet());
+				testRun.removeTestCases();
+			}
+			treeViewer.setInput(input);
+		}
 	}
 	
-	private void handleChildren(TestCase testCase, ResourceSet resourceSet) {
-//		for(TestCase childTestCase : testCase.getChildren()) {
-//			// handle only test groups
-//			if(!childTestCase.getChildren().isEmpty() && childTestCase.getParent().getAdapter(TestRunAdapter.class) == null) {
-//				appendAdapter(childTestCase, resourceSet);
-//			} else {
-//				handleChildren(childTestCase, resourceSet);
-//			}
-//		}
-	}
-	
-	private void appendAdapter(TestCase testCase, ResourceSet resourceSet) {
-//		URI sourceUri = testCase.getSourceUri();
-//		if(sourceUri != null) { 
-//			EObject eObject = resourceSet.getEObject(sourceUri, true);
-//			if(Status.FAILURE == testCase.getStatus()) {
-//				((TestCaseAdapter)TestCaseAdapterFactory.INSTANCE.adapt(eObject, TestCaseAdapter.class)).setTestCase(testCase);
-//			}
-//		}
+	private void handleChildren(TestGroup testGroup, ResourceSet resourceSet) {
+		for(TestCase testCase : testGroup.getTestCases()) {
+			if(testCase instanceof TestGroup) {
+				handleChildren((TestGroup)testCase, resourceSet);
+			} else {
+				TestGroup parent = (TestGroup)testCase.getParent();
+				URI sourceUri = parent.getSourceURI();
+				if(sourceUri != null) { 
+					EObject eObject = resourceSet.getEObject(sourceUri, true);
+					if(Status.FAILURE == testCase.getStatus()) {
+						((TestCaseAdapter)TestCaseAdapterFactory.INSTANCE.adapt(eObject, TestCaseAdapter.class)).setTestCase(testCase);
+					}
+				}
+				break;
+			}
+		}
 	}
 }
