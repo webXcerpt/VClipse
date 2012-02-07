8ba75d5456280fc0cbe486b52b16f5ae348e2642
AS <as@webxcerpt.com>
2012-02-07 10:28:10 +0000

    some improvements in test hierarchy

--- plugins/org.vclipse.vcml.diff.tests/src/org/vclipse/vcml/diff/tests/DiffTest.java
+++ plugins/org.vclipse.vcml.diff.tests/src/org/vclipse/vcml/diff/tests/DiffTest.java
@@ -2,35 +2,20 @@ package org.vclipse.vcml.diff.tests;
 
 import static org.junit.Assert.assertTrue;
 
-import org.eclipse.xtext.junit4.InjectWith;
-import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
 import org.eclipselabs.xtext.utils.unittesting.XtextTest;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
-@RunWith(XtextRunner2.class)
-@InjectWith(VcmlDiffInjectorProvider.class)
-public class DiffTest extends XtextTest {
+public abstract class DiffTest extends XtextTest {
 
-	public DiffTest() {
-		super(DiffTest.class.getSimpleName());
+	public DiffTest(String resourceRoot) {
+		super(resourceRoot == null ? DiffTest.class.getSimpleName() : resourceRoot);
 	}
 	
-	@Test
-	public void newSimpleCharacteristicTest() {
-		ignoreSerializationDifferences();
-		testFile("firstversion.vcml");
-		testFile("secondversion.vcml");
-		testFile("secondversion_diff.vcml");
-		testExistingFiles("firstversion.vcml", "secondversion.vcml", "secondversion_diff.vcml");
-	}
-
-	protected void testExistingFiles(String firstVersion, String secondVersion, String resultVersion) {
+	public void testExistingFiles(String firstVersion, String secondVersion, String resultVersion) {
 		assertTrue(replaceBotheringStrings(loadFileContents(resourceRoot, secondVersion)).equals(
 				replaceBotheringStrings(loadFileContents(resourceRoot, resultVersion), "import \"" + firstVersion + "\"")));
 	}
 	
-	protected String replaceBotheringStrings(String targetString, String ... additionalReplacements) {
+	public String replaceBotheringStrings(String targetString, String ... additionalReplacements) {
 		for(String part : additionalReplacements) {
 			targetString = targetString.replace(part, "");
 		}
---
+++ plugins/org.vclipse.vcml.diff.tests/src/org/vclipse/vcml/diff/tests/DiffTestsCollection.java
@@ -0,0 +1,24 @@
+package org.vclipse.vcml.diff.tests;
+
+import org.eclipse.xtext.junit4.InjectWith;
+import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(XtextRunner2.class)
+@InjectWith(VcmlDiffInjectorProvider.class)
+public class DiffTestsCollection extends DiffTest {
+
+	public DiffTestsCollection() {
+		super(DiffTestsCollection.class.getSimpleName());
+	}
+	
+	@Test
+	public void newSimpleCharacteristicTest() {
+		ignoreSerializationDifferences();
+		testFile("firstversion.vcml");
+		testFile("secondversion.vcml");
+		testFile("secondversion_diff.vcml");
+		testExistingFiles("firstversion.vcml", "secondversion.vcml", "secondversion_diff.vcml");
+	}
+}
