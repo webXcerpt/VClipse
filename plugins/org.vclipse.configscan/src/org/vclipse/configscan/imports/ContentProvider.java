703f0b39723c1d647adba4a19fe9a7639b1e2be2
AS <as@webxcerpt.com>
2012-02-16 10:51:30 +0000

    bug fix

--- plugins/org.vclipse.configscan/src/org/vclipse/configscan/imports/ContentProvider.java
+++ plugins/org.vclipse.configscan/src/org/vclipse/configscan/imports/ContentProvider.java
@@ -1,8 +1,10 @@
 package org.vclipse.configscan.imports;
 
+import java.io.File;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.viewers.IStructuredContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.Viewer;
@@ -11,16 +13,25 @@ import com.google.common.collect.Lists;
 
 class ContentProvider implements IStructuredContentProvider {
 	
-	private List<?> selectedFiles;
+	private List<File> selectedFiles;
 
 	public ContentProvider() {
 		selectedFiles = Lists.newArrayList();
 	}
 
+	// we work on File type !
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
 		if(oldInput != newInput) {
 			if(newInput instanceof IStructuredSelection) {
-				selectedFiles = Lists.newArrayList((Iterator<?>) ((IStructuredSelection)newInput).iterator());
+				Iterator<?> iterator = ((IStructuredSelection)newInput).iterator();
+				while(iterator.hasNext()) {
+					Object next = iterator.next();
+					if(next instanceof File) {
+						selectedFiles.add((File)next);
+					} else if(next instanceof IFile) {
+						selectedFiles.add(((IFile)next).getLocation().toFile());
+					}
+				}
 			}
 		}
 	}
