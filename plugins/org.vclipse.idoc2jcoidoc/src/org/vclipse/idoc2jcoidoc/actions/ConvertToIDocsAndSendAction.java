4041649a85f53f6b1bca3e573ba51bfeb6d49840
AS <as@webxcerpt.com>
2012-02-23 08:13:42 +0000

    implemented one click deployment to sap system, not yet tested

--- plugins/org.vclipse.idoc2jcoidoc/src/org/vclipse/idoc2jcoidoc/actions/ConvertToIDocsAndSendAction.java
+++ plugins/org.vclipse.idoc2jcoidoc/src/org/vclipse/idoc2jcoidoc/actions/ConvertToIDocsAndSendAction.java
@@ -1,38 +1,31 @@
 package org.vclipse.idoc2jcoidoc.actions;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
-import java.util.zip.ZipOutputStream;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.xtext.resource.XtextResourceSet;
-import org.eclipse.xtext.util.StringInputStream;
-import org.vclipse.idoc.iDoc.Model;
+import org.vclipse.connection.IConnectionHandler;
 import org.vclipse.idoc2jcoidoc.IIDoc2JCoIDocProcessor;
+import org.vclipse.idoc2jcoidoc.internal.RFCIDocsSender;
+import org.vclipse.vcml.vcml.Model;
+import org.vclipse.vcml2idoc.builder.VCML2IDocSwitch;
 
 import com.google.inject.Inject;
 import com.sap.conn.idoc.IDocDocument;
-import com.sap.conn.idoc.IDocXMLProcessor;
-import com.sap.conn.idoc.jco.JCoIDoc;
-import com.sap.conn.idoc.rt.xml.DefaultIDocXMLProcessor;
 import com.sap.conn.jco.JCoException;
 
 /**
@@ -43,6 +36,12 @@ public class ConvertToIDocsAndSendAction implements IObjectActionDelegate {
 	@Inject
 	private IIDoc2JCoIDocProcessor idocProcessor;
 	
+	@Inject
+	private VCML2IDocSwitch vcml2IDoc;
+	
+	@Inject
+	private IConnectionHandler connectionHandler;
+	
 	private IStructuredSelection selection;
 	
 	public void run(IAction action) {
@@ -51,9 +50,29 @@ public class ConvertToIDocsAndSendAction implements IObjectActionDelegate {
 			protected IStatus run(IProgressMonitor monitor) {
 				monitor.beginTask("Convert to IDocs and send.", IProgressMonitor.UNKNOWN);
 				IFile file = (IFile)selection.getFirstElement();
-				monitor.subTask("Converting to idocs..." + file.getName());
-				monitor.subTask("Converting idocs in jco idocs...");
-				monitor.subTask("Sending to SAP...");
+				XtextResourceSet xtextResourceSet = new XtextResourceSet();
+				Resource resource = xtextResourceSet.getResource(URI.createURI(file.getLocation().toString()), true);
+				EList<EObject> contents = resource.getContents();
+				if(!contents.isEmpty()) {
+					EObject object = contents.get(0);
+					if(object instanceof Model) {
+						Model vcmlModel = (Model)object;
+						try {
+							monitor.subTask("Converting to idocs..." + file.getName());
+							org.vclipse.idoc.iDoc.Model idocModel = vcml2IDoc.vcml2IDocs(vcmlModel);
+							monitor.subTask("Converting idocs in jco idocs...");
+							List<IDocDocument> idocs = idocProcessor.transform(idocModel , monitor);
+							monitor.subTask("Sending to SAP...");
+							IStatus sendStatus = new RFCIDocsSender().send(idocs, connectionHandler, monitor);
+							monitor.done();
+							return sendStatus;
+						} catch (JCoException exception) {
+							exception.printStackTrace();
+						} catch (CoreException exception) {
+							exception.printStackTrace();
+						}
+					}
+				}
 				return Status.OK_STATUS;
 			}
 		};
