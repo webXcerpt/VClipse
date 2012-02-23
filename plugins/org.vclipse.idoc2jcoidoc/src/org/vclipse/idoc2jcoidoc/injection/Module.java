1f2ddb18c24c674e54fa845b94e79a80d3f26062
Tim Geisler <tg@webxcerpt.com>
2012-02-23 09:37:53 +0000

    fixed injection problems

--- plugins/org.vclipse.idoc2jcoidoc/src/org/vclipse/idoc2jcoidoc/injection/Module.java
+++ plugins/org.vclipse.idoc2jcoidoc/src/org/vclipse/idoc2jcoidoc/injection/Module.java
@@ -10,6 +10,12 @@ import org.vclipse.connection.IConnectionHandler;
 import org.vclipse.connection.VClipseConnectionPlugin;
 import org.vclipse.idoc2jcoidoc.IIDoc2JCoIDocProcessor;
 import org.vclipse.idoc2jcoidoc.internal.DefaultIDoc2JCoIDocProcessor;
+import org.vclipse.vcml.ui.VCMLUiPlugin;
+import org.vclipse.vcml2idoc.VCML2IDocUIPlugin;
+import org.vclipse.vcml2idoc.builder.VCML2IDocSwitch;
+
+import com.google.inject.Binder;
+import com.google.inject.name.Names;
 
 /**
  *
@@ -26,6 +32,12 @@ public class Module extends AbstractGenericModule {
 		return plugin.getPreferenceStore();
 	}
 	
+	@Override
+	public void configure(Binder binder) {
+		super.configure(binder);
+		binder.bind(IPreferenceStore.class).annotatedWith(Names.named(VCMLUiPlugin.ID)).toInstance(VCMLUiPlugin.getDefault().getInjector().getInstance(IPreferenceStore.class));
+	}
+
 	public IConnectionHandler bindConnectionHandler() {
 		return VClipseConnectionPlugin.getDefault().getInjector().getInstance(IConnectionHandler.class);
 	}
@@ -33,4 +45,9 @@ public class Module extends AbstractGenericModule {
 	public Class<? extends IIDoc2JCoIDocProcessor> bindIDoc2JCoIDocProcessor() {
 		return DefaultIDoc2JCoIDocProcessor.class;
 	}
+
+	public VCML2IDocSwitch bindVCML2IDocSwitch() {
+		return VCML2IDocUIPlugin.getDefault().getInjector().getInstance(VCML2IDocSwitch.class);
+	}
+	
 }
