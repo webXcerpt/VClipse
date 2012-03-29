48b2b30481186fe6393b48d0c898bb0ec06b4e57
AS <as@webxcerpt.com>
2012-03-29 07:21:51 +0000

    added hover info provider for characteristic values

--- plugins/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/editor/hover/VCMLHoverProvider.java
+++ plugins/org.vclipse.vcml.ui/src/org/vclipse/vcml/ui/editor/hover/VCMLHoverProvider.java
@@ -17,6 +17,10 @@ import org.vclipse.base.naming.IClassNameProvider;
 import org.vclipse.vcml.documentation.VCMLAdditionalInformationProvider;
 import org.vclipse.vcml.documentation.VCMLDescriptionProvider;
 import org.vclipse.vcml.documentation.VCMLDocumentationProvider;
+import org.vclipse.vcml.vcml.Characteristic;
+import org.vclipse.vcml.vcml.CharacteristicType;
+import org.vclipse.vcml.vcml.CharacteristicValue;
+import org.vclipse.vcml.vcml.SymbolicType;
 
 import com.google.inject.Inject;
 
@@ -50,31 +54,47 @@ public class VCMLHoverProvider extends DefaultEObjectHoverProvider {
 		return classNameProvider;
 	}
 
-	protected String getHoverInfoAsHtml(EObject o) {
-		if (!hasHover(o))
+	protected String getHoverInfoAsHtml(EObject object) {
+		if(!hasHover(object))
 			return null;
+		
 		StringBuffer buffer = new StringBuffer();
-		buffer.append(getFirstLine(o));
-		String description = descriptionProvider.getDocumentation(o);
+		buffer.append(getFirstLine(object));
+		String description = descriptionProvider.getDocumentation(object);
 		if (description!=null && description.length()>0) {
 			buffer.append("<br/>");
 			buffer.append(description);
 		}
-		String documentation = documentationProvider.getDocumentation(o);
+		String documentation = documentationProvider.getDocumentation(object);
 		if (documentation!=null && documentation.length()>0) {
 			buffer.append("<br/>");
 			buffer.append(documentation);
 		}
-		String additionalInformation = additionalInformationProvider.getDocumentation(o);
+		String additionalInformation = additionalInformationProvider.getDocumentation(object);
 		if (additionalInformation!=null && additionalInformation.length()>0) {
 			buffer.append("<br/>");
 			buffer.append(additionalInformation);
 		}
-		String multilineCommentDocumentation = getDocumentation(o);
+		String multilineCommentDocumentation = getDocumentation(object);
 		if (multilineCommentDocumentation!=null && multilineCommentDocumentation.length()>0) {
 			buffer.append("<p>");
 			buffer.append(multilineCommentDocumentation);
 			buffer.append("</p>");
+		}
+		if(object instanceof Characteristic) {
+			Characteristic characteristic = (Characteristic)object;
+			CharacteristicType type = characteristic.getType();
+			if(type instanceof SymbolicType) {
+				SymbolicType symbolicType = (SymbolicType)type;
+				if(!symbolicType.getValues().isEmpty()) {
+					buffer.append("<br/><br/>Values:<ul>");
+					for(CharacteristicValue value : symbolicType.getValues()) {
+						buffer.append("<li><b>" + value.getName() + "</b>" 
+								/*+ (value.getDescription()==null ? "" : ": <em>" + value.getDescription() + "</em>")*/ + "</li>");
+					}
+					buffer.append("</ul>");					
+				}
+			}
 		}
 		return buffer.toString();
 	}
