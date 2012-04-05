0bf1cce6e579e28bd8bd21b5369a9d5f09e6992c
AS <as@webxcerpt.com>
2012-04-05 13:44:44 +0000

    changed name handling for characteristic values

--- plugins/org.vclipse.vcml/src/org/vclipse/vcml/naming/VcmlNamesAreUniqueValidationHelper.java
+++ plugins/org.vclipse.vcml/src/org/vclipse/vcml/naming/VcmlNamesAreUniqueValidationHelper.java
@@ -2,11 +2,15 @@ package org.vclipse.vcml.naming;
 
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.xtext.validation.NamesAreUniqueValidationHelper;
+import org.vclipse.vcml.vcml.VcmlPackage;
 
 public class VcmlNamesAreUniqueValidationHelper extends NamesAreUniqueValidationHelper {
 
 	@Override
 	protected EClass getAssociatedClusterType(EClass eClass) {
+		if(eClass == VcmlPackage.eINSTANCE.getCharacteristicValue()) {
+			return null;
+		}
 		return eClass;
 	}
 }
