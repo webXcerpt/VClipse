e24faa52721461f7b3243287f769ae36c4667bee
AS <as@webxcerpt.com>
2012-04-11 13:42:55 +0000

    bug fix for NullPointerException which is raised because null is returned for characteristic values

--- plugins/org.vclipse.vcml/src/org/vclipse/vcml/naming/VcmlNamesAreUniqueValidationHelper.java
+++ plugins/org.vclipse.vcml/src/org/vclipse/vcml/naming/VcmlNamesAreUniqueValidationHelper.java
@@ -1,16 +1,56 @@
 package org.vclipse.vcml.naming;
 
+import java.util.Map;
+
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.validation.NamesAreUniqueValidationHelper;
+import org.eclipse.xtext.validation.ValidationMessageAcceptor;
 import org.vclipse.vcml.vcml.VcmlPackage;
+
+import com.google.common.collect.Maps;
 
 public class VcmlNamesAreUniqueValidationHelper extends NamesAreUniqueValidationHelper {
 
 	private static final VcmlPackage VCML_PACKAGE = VcmlPackage.eINSTANCE;
 
 	@Override
+	protected void checkDescriptionForDuplicatedName(
+			IEObjectDescription description,
+			Map<EClass, Map<QualifiedName, IEObjectDescription>> clusterTypeToName,
+			ValidationMessageAcceptor acceptor) {
+		EObject object = description.getEObjectOrProxy();
+		EClass eClass = object.eClass();
+		QualifiedName qualifiedName = description.getName();
+		EClass clusterType = getAssociatedClusterType(eClass);
+		if(clusterType == null) {
+			return;
+		}
+		Map<QualifiedName, IEObjectDescription> nameToDescription = clusterTypeToName.get(clusterType);
+		if (nameToDescription == null) {
+			nameToDescription = Maps.newHashMap();
+			nameToDescription.put(qualifiedName, description);
+			clusterTypeToName.put(clusterType, nameToDescription);
+		} else {
+			if (nameToDescription.containsKey(qualifiedName)) {
+				IEObjectDescription prevDescription = nameToDescription.get(qualifiedName);
+				if (prevDescription != null) {
+					createDuplicateNameError(prevDescription, clusterType, acceptor);
+					nameToDescription.put(qualifiedName, null);
+				}
+				createDuplicateNameError(description, clusterType, acceptor);
+			} else {
+				nameToDescription.put(qualifiedName, description);
+			}
+		}
+	}
+	
+	@Override
 	protected EClass getAssociatedClusterType(EClass eClass) {
 		if(eClass == VCML_PACKAGE.getCharacteristicValue() || eClass == VCML_PACKAGE.getConstraintClass()) {
+			// do not handle names for this types
 			return null;
 		}
 		return eClass;
