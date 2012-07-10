a828fa7ac25e5bc426d6cb275e69c506db2dc0ae
AS <as@webxcerpt.com>
2012-07-10 09:35:12 +0000

    simplified implementation

--- plugins/org.vclipse.dependency/src/org/vclipse/dependency/formatting/SourceCommentHandlingStreamer.java
+++ plugins/org.vclipse.dependency/src/org/vclipse/dependency/formatting/SourceCommentHandlingStreamer.java
@@ -3,10 +3,6 @@ package org.vclipse.dependency.formatting;
 import java.io.IOException;
 import java.util.List;
 
-import org.eclipse.xtext.AbstractElement;
-import org.eclipse.xtext.Group;
-import org.eclipse.xtext.Keyword;
-import org.eclipse.xtext.TerminalRule;
 import org.eclipse.xtext.formatting.impl.NodeModelStreamer;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.ILeafNode;
@@ -21,8 +17,6 @@ import com.google.inject.Inject;
 
 public class SourceCommentHandlingStreamer extends NodeModelStreamer {
 
-	private static final String SOURCE_COMMENT_START = "*";
-	
 	@Inject
 	private DependencyGrammarAccess grammarAccess;
 	
@@ -35,11 +29,11 @@ public class SourceCommentHandlingStreamer extends NodeModelStreamer {
 			((ITokenStreamExtension) out).init(findRootRuleForRegion(nodes.get(0)));
 		boolean lastIsTokenOrComment = false;
 		for (INode node : nodes) {
-			boolean currentIsTokenOrComment = tokenUtil.isCommentNode(node) || tokenUtil.isToken(node);
-			if(isCommentStart(node)) {
-				writeCommentPrefix(out, node);
+			if(tokenUtil.isCommentNode(node)) {
+				out.writeHidden(grammarAccess.getSL_COMMENTRule(), "\n" + node.getText());
 				continue;
 			}
+			boolean currentIsTokenOrComment = tokenUtil.isCommentNode(node) || tokenUtil.isToken(node);
 			if (lastIsTokenOrComment && currentIsTokenOrComment)
 				writeHiddenEmpty(out);
 			lastIsTokenOrComment = currentIsTokenOrComment;
@@ -56,28 +50,5 @@ public class SourceCommentHandlingStreamer extends NodeModelStreamer {
 		int rStart = nodes.get(0).getOffset();
 		int rLength = (nodes.get(nodes.size() - 1).getOffset() + nodes.get(nodes.size() - 1).getLength()) - rStart;
 		return new TextRegion(rStart, rLength);
-	}
-	
-	private void writeCommentPrefix(ITokenStream out, INode node) throws IOException {
-		TerminalRule slComment = grammarAccess.getSL_COMMENTRule();
-		AbstractElement alternatives = slComment.getAlternatives();
-		if(alternatives instanceof Group) {
-			for(AbstractElement element : ((Group)alternatives).getElements()) {
-				if(element instanceof Keyword) {
-					String value = ((Keyword)element).getValue();
-					if(tokenUtil.isCommentNode(node)) {
-						out.writeHidden(slComment, "\n" + node.getText());
-						break;
-					} else if(SOURCE_COMMENT_START.equals(value)) {
-						out.writeHidden(slComment, "\n" + value);		
-						break;
-					}
-				}
-			}
-		}
-	}
-	
-	private boolean isCommentStart(INode node) {
-		return node.getText().startsWith(SOURCE_COMMENT_START);
 	}
 }
