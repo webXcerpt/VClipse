0b77a20d05853a34d385a9f5df98997cf0c2505c
AS <as@webxcerpt.com>
2012-07-25 11:49:12 +0000

    bug fix for variant table content

--- plugins/org.vclipse.vcml.diff/src/org/vclipse/vcml/diff/engines/VcmlSimilarityCheckerId.java
+++ plugins/org.vclipse.vcml.diff/src/org/vclipse/vcml/diff/engines/VcmlSimilarityCheckerId.java
@@ -5,10 +5,11 @@ import org.eclipse.emf.compare.match.engine.internal.EcoreIDSimilarityChecker;
 import org.eclipse.emf.compare.match.statistic.MetamodelFilter;
 import org.eclipse.emf.ecore.EObject;
 import org.vclipse.vcml.vcml.VCObject;
+import org.vclipse.vcml.vcml.VariantTableContent;
 
 @SuppressWarnings("restriction")
 public class VcmlSimilarityCheckerId extends EcoreIDSimilarityChecker {
-
+	
 	public VcmlSimilarityCheckerId(MetamodelFilter filter, AbstractSimilarityChecker checker) {
 		super(filter, checker);
 	}
@@ -16,7 +17,9 @@ public class VcmlSimilarityCheckerId extends EcoreIDSimilarityChecker {
 	@Override
 	protected String computeID(EObject object) {
 		if(object instanceof VCObject) {
-			return object.getClass().getName().concat(((VCObject)object).getName());
+			return object.getClass().getName().concat(
+					(object instanceof VariantTableContent) ? 
+							((VariantTableContent)object).getTable().getName() : ((VCObject)object).getName());
 		}
 		return null;
 	}
