df6df97ea69b1ac0b8011ab2d022a0a11e46f1b7
AS <as@webxcerpt.com>
2012-09-05 11:11:21 +0000

    added code in evaluation

--- plugins/org.vclipse.vcml/src/org/vclipse/vcml/refactoring/VCMLCustomisation.java
+++ plugins/org.vclipse.vcml/src/org/vclipse/vcml/refactoring/VCMLCustomisation.java
@@ -1,20 +1,34 @@
 package org.vclipse.vcml.refactoring;
 
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.xtext.EcoreUtil2;
 import org.vclipse.refactoring.core.IRefactoringContext;
 import org.vclipse.refactoring.core.RefactoringCustomisation;
 import org.vclipse.vcml.vcml.CharacteristicReference_P;
+import org.vclipse.vcml.vcml.ConditionalConstraintRestriction;
+import org.vclipse.vcml.vcml.ConstraintRestriction;
 import org.vclipse.vcml.vcml.ConstraintSource;
 import org.vclipse.vcml.vcml.InCondition_C;
 import org.vclipse.vcml.vcml.Literal;
 import org.vclipse.vcml.vcml.MDataCharacteristic_P;
 import org.vclipse.vcml.vcml.PFunction;
 
+import com.google.inject.Inject;
+
 public class VCMLCustomisation extends RefactoringCustomisation {
+	
+	@Inject
+	private VCMLRefactoring vcmlRefactoring;
 	
 	public boolean evaluate_Extract_ConstraintSource(IRefactoringContext context, ConstraintSource reference) {
 		context.setText("Extract common conditions from restrictions");
-		return Boolean.TRUE;
+		ConstraintSource source = EcoreUtil2.getContainerOfType(context.getSourceElement(), ConstraintSource.class);
+		for(ConstraintRestriction restriction : source.getRestrictions()) {
+			if(restriction instanceof ConditionalConstraintRestriction) {
+				return Boolean.TRUE;
+			}
+		}
+		return Boolean.FALSE;
 	}
 	
 	public boolean evaluate_Inline_ConstraintSource(IRefactoringContext context, ConstraintSource source) {
