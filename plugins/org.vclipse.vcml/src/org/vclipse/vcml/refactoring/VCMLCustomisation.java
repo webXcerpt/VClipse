d52d2b7756481eb9e96c746f66977b4e829f5540
AS <as@webxcerpt.com>
2012-09-11 07:37:57 +0000

    removed not required code

--- plugins/org.vclipse.vcml/src/org/vclipse/vcml/refactoring/VCMLCustomisation.java
+++ plugins/org.vclipse.vcml/src/org/vclipse/vcml/refactoring/VCMLCustomisation.java
@@ -1,6 +1,9 @@
 package org.vclipse.vcml.refactoring;
 
+import java.util.List;
+
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EStructuralFeature;
 import org.vclipse.refactoring.core.IRefactoringContext;
 import org.vclipse.refactoring.core.RefactoringCustomisation;
 import org.vclipse.vcml.vcml.CharacteristicReference_P;
@@ -11,6 +14,7 @@ import org.vclipse.vcml.vcml.InCondition_C;
 import org.vclipse.vcml.vcml.Literal;
 import org.vclipse.vcml.vcml.MDataCharacteristic_P;
 import org.vclipse.vcml.vcml.PFunction;
+import org.vclipse.vcml.vcml.VcmlPackage;
 
 import com.google.inject.Inject;
 
@@ -18,6 +22,12 @@ public class VCMLCustomisation extends RefactoringCustomisation {
 	
 	@Inject
 	private VCMLRefactoring vcmlRefactoring;
+	
+	protected static VcmlPackage VCML_PACKAGE = VcmlPackage.eINSTANCE;
+	
+	public List<? extends EStructuralFeature> features_Extract_ConstraintSource(IRefactoringContext context) {
+		return get(VCML_PACKAGE.getConstraintSource_Restrictions());
+	}
 	
 	public boolean evaluate_Extract_ConstraintSource(IRefactoringContext context, ConstraintSource source) {
 		context.setLabel("Extract common conditions from restrictions");
