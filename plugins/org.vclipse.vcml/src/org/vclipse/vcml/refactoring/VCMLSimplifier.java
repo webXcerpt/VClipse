c2ee234b708bdbc6c6d65dc05d9eac6e34bc91fb
AS <as@webxcerpt.com>
2012-10-08 14:20:09 +0000

    enabled a re-factoring

--- plugins/org.vclipse.vcml/src/org/vclipse/vcml/refactoring/VCMLSimplifier.java
+++ plugins/org.vclipse.vcml/src/org/vclipse/vcml/refactoring/VCMLSimplifier.java
@@ -8,6 +8,7 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.EcoreUtil2;
 import org.vclipse.refactoring.IRefactoringContext;
 import org.vclipse.refactoring.core.RefactoringExecuter;
+import org.vclipse.vcml.vcml.Comparison;
 import org.vclipse.vcml.vcml.Condition;
 import org.vclipse.vcml.vcml.ConditionalConstraintRestriction;
 import org.vclipse.vcml.vcml.ConstraintRestriction;
@@ -22,6 +23,10 @@ public class VCMLSimplifier extends RefactoringExecuter {
 	protected final VcmlFactory VCML_FACTORY = VcmlFactory.eINSTANCE;
 	protected final VcmlPackage VCML_PACKAGE = VcmlPackage.eINSTANCE;
 	
+	/*
+	 * Re-factoring: extracts common conditions from restrictions to a 
+	 * condition part of a constraint source. 
+	 */
 	public void refactoring_Extract_ConstraintSource(IRefactoringContext context) {
 		EObject sourceElement = context.getSourceElement();
 		sourceElement = EcoreUtil2.getContainerOfType(sourceElement, ConstraintSource.class);
@@ -67,40 +72,40 @@ public class VCMLSimplifier extends RefactoringExecuter {
 		}
 	}
 	
-//	
-//	public List<? extends EObject> refactoring_Inline_ConstraintSource(IRefactoringContext context) {
-//		List<EObject> changes = Lists.newArrayList();
-//		inlineCondition(EcoreUtil2.getContainerOfType(context.getSourceElement(), ConstraintSource.class), changes);
-//		return changes;
-//	}
+	/*
+	 * Re-factoring: executes inline operation for each restriction 
+	 */
+	public void refactoring_Inline_ConstraintSource(IRefactoringContext context) {
+		EObject element = context.getSourceElement();
+		ConstraintSource source = EcoreUtil2.getContainerOfType(element, ConstraintSource.class);
+		inline_Condition(source);
+		context.setSourceElement(source);
+	}
 	
-//	public void inlineCondition(ConstraintSource source, List<EObject> changes) {
-//		Comparison previous = null;
-//		EList<ConstraintRestriction> restrictions = source.getRestrictions();
-//		for(ConstraintRestriction restriction : restrictions) {
-//			if(previous == null) {
-//				if(!(restriction instanceof Comparison)) {
-//					break;
-//				}
-//				previous = (Comparison)restriction;
-//				continue;
-//			} 
-//		}
-//		if(previous != null) {
-//			Condition condition = source.getCondition();
-//			changes.add(getChangeDescription(source, VCML_PACKAGE.getConstraintSource_Condition(), null));
-//			EList<ConstraintRestriction> newRestrictions = new BasicEList<ConstraintRestriction>();
-//			for(ConstraintRestriction restriction : restrictions) {
-//				ConditionalConstraintRestriction ccr = VCML_FACTORY.createConditionalConstraintRestriction();
-//				ccr.setCondition(EcoreUtil.copy(condition));
-//				ccr.setRestriction(EcoreUtil.copy(restriction));
-//				newRestrictions.add(ccr);
-//			}
-//			ListDifferenceAnalyzer analyzer = new ListDifferenceAnalyzer();
-//			for(ListChange listChange : analyzer.analyzeLists(restrictions, newRestrictions)) {
-//				listChange.applyAndReverse(getObjectList(restrictions));
-//				changes.add(listChange);
-//			}
-//		}
-//	}
+	public void inline_Condition(ConstraintSource source) {
+		Comparison previous = null;
+		EList<ConstraintRestriction> restrictions = source.getRestrictions();
+		for(ConstraintRestriction restriction : restrictions) {
+			if(previous == null) {
+				if(!(restriction instanceof Comparison)) {
+					break;
+				}
+				previous = (Comparison)restriction;
+				continue;
+			} 
+		}
+		if(previous != null) {
+			Condition condition = source.getCondition();
+			source.setCondition(null);
+			List<ConstraintRestriction> newRestrictions = Lists.newArrayList();
+			for(ConstraintRestriction restriction : restrictions) {
+				ConditionalConstraintRestriction ccr = VCML_FACTORY.createConditionalConstraintRestriction();
+				ccr.setCondition(EcoreUtil.copy(condition));
+				ccr.setRestriction(EcoreUtil.copy(restriction));
+				newRestrictions.add(ccr);
+			}
+			restrictions.clear();
+			restrictions.addAll(newRestrictions);
+		}
+	}
 }
