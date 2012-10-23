ebc5381a4e25b1a114892792a067166ba9e57f19
AS <as@webxcerpt.com>
2012-10-23 10:36:08 +0000

    removed not required code

--- plugins/org.vclipse.refactoring/src/org/vclipse/refactoring/IRefactoringExecuter.java
+++ plugins/org.vclipse.refactoring/src/org/vclipse/refactoring/IRefactoringExecuter.java
@@ -5,12 +5,9 @@ import java.util.Set;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.EPackage;
 
 public interface IRefactoringExecuter {
 
-	public EPackage getPackage();
-	
 	public Set<EClass> getTopLevelTypes();
 	
 	public void refactor(IRefactoringContext context) throws CoreException;
