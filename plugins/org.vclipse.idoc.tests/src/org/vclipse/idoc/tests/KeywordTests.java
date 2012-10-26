3591ab7748493430b6053409a70079821274919d
AS <as@webxcerpt.com>
2012-10-26 07:16:17 +0000

    adapted test project to the current version of the unit test framework

--- plugins/org.vclipse.idoc.tests/src/org/vclipse/idoc/tests/KeywordTests.java
+++ plugins/org.vclipse.idoc.tests/src/org/vclipse/idoc/tests/KeywordTests.java
@@ -1,13 +1,13 @@
 package org.vclipse.idoc.tests;
 
 import org.eclipse.xtext.junit4.InjectWith;
-import org.eclipselabs.xtext.utils.unittesting.XtextRunner2;
+import org.eclipse.xtext.junit4.XtextRunner;
 import org.eclipselabs.xtext.utils.unittesting.XtextTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.vclipse.idoc.IDocInjectorProvider;
 
-@RunWith(XtextRunner2.class)
+@RunWith(XtextRunner.class)
 @InjectWith(IDocInjectorProvider.class)
 public class KeywordTests extends XtextTest {
 
