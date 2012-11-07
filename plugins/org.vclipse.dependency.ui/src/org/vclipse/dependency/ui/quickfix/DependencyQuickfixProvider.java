b3f56f21963c83b4ad7837b5393ed8bacb248388
AS <as@webxcerpt.com>
2012-11-07 08:59:00 +0000

    implemented extension for vcobject creation

--- plugins/org.vclipse.dependency.ui/src/org/vclipse/dependency/ui/quickfix/DependencyQuickfixProvider.java
+++ plugins/org.vclipse.dependency.ui/src/org/vclipse/dependency/ui/quickfix/DependencyQuickfixProvider.java
@@ -3,24 +3,36 @@ package org.vclipse.dependency.ui.quickfix;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
 
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EGenericType;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.xtext.resource.SaveOptions;
 import org.eclipse.xtext.ui.editor.model.edit.IModificationContext;
 import org.eclipse.xtext.ui.editor.model.edit.ISemanticModification;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
 import org.eclipse.xtext.ui.editor.quickfix.Fix;
 import org.eclipse.xtext.ui.editor.quickfix.IssueResolutionAcceptor;
+import org.eclipse.xtext.util.SimpleAttributeResolver;
 import org.eclipse.xtext.validation.Issue;
 import org.vclipse.vcml.utils.DependencySourceUtils;
+import org.vclipse.vcml.vcml.ConstraintClass;
+import org.vclipse.vcml.vcml.ConstraintMaterial;
+import org.vclipse.vcml.vcml.ConstraintObject;
+import org.vclipse.vcml.vcml.ObjectCharacteristicReference;
 import org.vclipse.vcml.vcml.VCObject;
 import org.vclipse.vcml.vcml.VcmlFactory;
 import org.vclipse.vcml.vcml.VcmlModel;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 import com.google.inject.Inject;
 
 public class DependencyQuickfixProvider extends DefaultQuickfixProvider {
@@ -39,10 +51,11 @@ public class DependencyQuickfixProvider extends DefaultQuickfixProvider {
 		String description = "Creates a new vcobject with name " + name;
 		acceptor.accept(issue, label, description, null, new ISemanticModification() {
 			public void apply(EObject element, IModificationContext context) throws Exception {
+				EObject locationContext = getContext(element);
 				ResourceSet resourceSet = element.eResource().getResourceSet();
 				URI uri = URI.createURI(fileUri);
 				Resource vcmlResource = resourceSet.getResource(uri, true);
-				createVCObject(name, type, vcmlResource);
+				createVCObject(name, type, vcmlResource, locationContext);
 			}
 		});
 	}
@@ -71,17 +84,27 @@ public class DependencyQuickfixProvider extends DefaultQuickfixProvider {
 		acceptor.accept(issue, label, description, null, new ISemanticModification() {
 			@Override
 			public void apply(EObject element, IModificationContext context) throws Exception {
+				EObject locationContext = getContext(element);
 				Resource eResource = element.eResource();
 				URI vcmlUri = sourceUtils.getVcmlResourceURI(eResource.getURI());
 				Resource vcmlResource = eResource.getResourceSet().getResource(vcmlUri, true);
 				if(vcmlUri != null) {
-					createVCObject(linkText, referenceType, vcmlResource);
+					createVCObject(linkText, referenceType, vcmlResource, locationContext);
 				}
 			}
 		});
 	}
 	
-	protected void createVCObject(final String linkText, final String referenceType, Resource resource) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, IOException  {
+	protected EObject getContext(EObject element) {
+		if(element instanceof ObjectCharacteristicReference) {
+			ConstraintObject location = ((ObjectCharacteristicReference)element).getLocation();
+			return location instanceof ConstraintClass ? ((ConstraintClass)location).getClass_() : ((ConstraintMaterial)location).getObjectType();
+		}
+		return null;
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected void createVCObject(final String linkText, final String referenceType, Resource resource, EObject locationContext) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException, IOException  {
 		EList<EObject> contents = resource.getContents();
 		if(!contents.isEmpty()) {
 			EObject topObject = contents.get(0);
@@ -93,6 +116,29 @@ public class DependencyQuickfixProvider extends DefaultQuickfixProvider {
 					VCObject vcobject = (VCObject)result;
 					vcobject.setName(linkText);
 					vcmlModel.getObjects().add(vcobject);
+	
+					if(locationContext != null) {
+						EClass type = locationContext.eClass();
+						EList<EReference> references = type.getEAllReferences();
+						for(EReference reference : references) {
+							EGenericType genericType = reference.getEGenericType();
+							if(genericType.getEClassifier() == vcobject.eClass()) {
+								if(reference.isMany()) {
+									EList<EObject> entries = (EList<EObject>)locationContext.eGet(reference);
+									final String name = vcobject.getName();
+									Iterator<EObject> iterator = Iterables.filter(entries, new Predicate<EObject>() {
+										public boolean apply(EObject object) {
+											String resolvedName = SimpleAttributeResolver.NAME_RESOLVER.getValue(object);
+											return name.equals(resolvedName);
+										}
+									}).iterator();
+									if(!iterator.hasNext()) {
+										entries.add(EcoreUtil.copy(vcobject));										
+									}
+								}
+							}
+						}
+					}				
 					resource.save(SaveOptions.defaultOptions().toOptionsMap());
 				}
 			}
