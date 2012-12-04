c13882bf74fef54ab5e7e6bed9429acba8092249
AS <as@webxcerpt.com>
2012-12-04 07:24:34 +0000

    added copyright comments

--- plugins/org.vclipse.vcml/src/org/vclipse/vcml/utils/DependencySourceUtils.java
+++ plugins/org.vclipse.vcml/src/org/vclipse/vcml/utils/DependencySourceUtils.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 webXcerpt Software GmbH.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *    webXcerpt Software GmbH - initial creator
+ ******************************************************************************/
 package org.vclipse.vcml.utils;
 
 import java.io.IOException;
