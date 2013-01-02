ee2aa46727571bd58189899fa37f02a44dbba652
AS <as@webxcerpt.com>
2013-01-02 08:42:47 +0000

    removed exception during id computation

--- plugins/org.vclipse.vcml.diff/src/org/vclipse/vcml/diff/engines/VcmlSimilarityCheckerId.java
+++ plugins/org.vclipse.vcml.diff/src/org/vclipse/vcml/diff/engines/VcmlSimilarityCheckerId.java
@@ -5,6 +5,7 @@ import org.eclipse.emf.compare.match.engine.internal.EcoreIDSimilarityChecker;
 import org.eclipse.emf.compare.match.statistic.MetamodelFilter;
 import org.eclipse.emf.ecore.EObject;
 import org.vclipse.vcml.vcml.VCObject;
+import org.vclipse.vcml.vcml.VariantTable;
 import org.vclipse.vcml.vcml.VariantTableContent;
 
 @SuppressWarnings("restriction")
@@ -17,9 +18,12 @@ public class VcmlSimilarityCheckerId extends EcoreIDSimilarityChecker {
 	@Override
 	protected String computeID(EObject object) {
 		if(object instanceof VCObject) {
-			return object.getClass().getName().concat(
-					(object instanceof VariantTableContent) ? 
-							((VariantTableContent)object).getTable().getName() : ((VCObject)object).getName());
+			String className = object.eClass().getInstanceClassName();
+			if(object instanceof VariantTableContent) {
+				VariantTable table = ((VariantTableContent)object).getTable();
+				return table == null ? className : className.concat(table.getName());
+			}
+			return className;
 		}
 		return null;
 	}
