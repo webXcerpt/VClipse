6192c6cbe5ea481c14afb3128ca568122be860d1
AS <as@webxcerpt.com>
2013-02-07 15:06:57 +0000

    added missing code

--- plugins/org.vclipse.vcml.ui.actions/src/org/vclipse/bapi/actions/characteristic/values/CreateChangeCharacteristicsValuesDependencies.java
+++ plugins/org.vclipse.vcml.ui.actions/src/org/vclipse/bapi/actions/characteristic/values/CreateChangeCharacteristicsValuesDependencies.java
@@ -47,9 +47,7 @@ public class CreateChangeCharacteristicsValuesDependencies extends BAPIActionHan
 			throw new BAPIException("Action \"Create/ change dependencies\" for values of a ch was canceled by the user.");
 		}
 		IProgressMonitor submonitor = SubMonitor.convert(monitor, "Creating/ changing dependencies for values of characteristic " + cstic.getName(), IProgressMonitor.UNKNOWN);
-		functionPerformer.beginTransaction();
 		functionPerformer.CAMA_CHAR_VAL_ALLOCAT_GLOB_DEP(cstic, submonitor, vcmlModel.getOptions(), cstic.getOptions());
-		functionPerformer.endTransaction();
 		submonitor.done();
 	}
 
