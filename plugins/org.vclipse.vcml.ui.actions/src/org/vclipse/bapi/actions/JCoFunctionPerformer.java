6399ee136fee33227ad8d1f1713251f5e17fc549
AS <as@webxcerpt.com>
2013-02-07 13:05:06 +0000

    added a function for maintaning global dependencies of the characteristics

--- plugins/org.vclipse.vcml.ui.actions/src/org/vclipse/bapi/actions/JCoFunctionPerformer.java
+++ plugins/org.vclipse.vcml.ui.actions/src/org/vclipse/bapi/actions/JCoFunctionPerformer.java
@@ -14,31 +14,87 @@ package org.vclipse.bapi.actions;
 import java.util.List;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.vclipse.base.naming.INameProvider;
+import org.vclipse.vcml.VCMLUtilities;
+import org.vclipse.vcml.vcml.Characteristic;
+import org.vclipse.vcml.vcml.Dependency;
 import org.vclipse.vcml.vcml.Option;
 
+import com.google.inject.Inject;
 import com.sap.conn.jco.JCoException;
 import com.sap.conn.jco.JCoFunction;
 import com.sap.conn.jco.JCoParameterList;
+import com.sap.conn.jco.JCoTable;
 
 /**
  *
  */
 public class JCoFunctionPerformer extends BAPIUtils {
 
+	/**
+	 * 
+	 */
+	private static final String FLDELETE = "FL_DELETE";
+
 	public static final String SELECTED = "X";
 	
 	/**
 	 * Values
 	 */
-	public static final String CHARACTERISTIC = "CHARACTERISTIC";
+	public static final String CHARACTERISTIC[] = new String[]{"CHARACTERISTIC", "CHARACT"};
 	public static final String DEPENDENCY = "DEPENDENCY";
 	public static final String DEP_TYPE = "DEP_TYPE";
 	public static final String DEPENDENCY_DATA = "DEPENDENCY_DATA";
+	
+	public static final String CHANGE_NO = "CHANGE_NO";
+	public static final String DATE = "DATE";
 	
 	/**
 	 * Tables
 	 */
 	public static final String DEP_ASSIGN = "DEP_ASSIGN";
+	
+	@Inject
+	private VCMLUtilities vcmlUtilities;
+	
+	@Inject
+	private INameProvider nameProvider;
+	
+	/**
+	 * 
+	 */
+	public JCoFunction CAMA_CHAR_SNGL_MAINTAIN(Characteristic cstic, IProgressMonitor monitor, List<Option> objectOptions, List<Option> modelOptions) throws JCoException {
+		JCoFunction function = getJCoFunction("CAMA_CHAR_SNGL_MAINTAIN", monitor);
+		JCoParameterList ipl = function.getImportParameterList();
+		applyOptions(modelOptions, objectOptions, ipl, JCoFunctionPerformer.CHANGE_NO, JCoFunctionPerformer.DATE);
+		ipl.setValue(CHARACTERISTIC[1], cstic.getName());
+		ipl.setValue(FLDELETE, SELECTED);
+		execute(function, monitor, "");
+		return function;
+	}
+	
+	/**
+	 * 
+	 */
+	public JCoFunction CAMA_CHAR_ALLOCATE_GLOB_DEP(Characteristic cstic, IProgressMonitor monitor, List<Option> global, List<Option> local) throws Exception {
+		if(monitor.isCanceled()) {
+			monitor.done();
+			throw new BAPIException("Function call CAMA_CHAR_ALLOCATE_GLOB_DEP cancelled by the user.");
+		}
+		JCoFunction function = getJCoFunction("CAMA_CHAR_ALLOCATE_GLOB_DEP", monitor);
+		JCoParameterList ipl = function.getImportParameterList();
+		applyOptions(global, local, ipl, JCoFunctionPerformer.CHANGE_NO, JCoFunctionPerformer.DATE);
+		JCoTable dependenciesTable = function.getTableParameterList().getTable("CHAR_DEP_ASSIGN");
+		String csticName = cstic.getName();
+		for(Dependency dependency : cstic.getDependencies().getDependencies()) {
+			dependenciesTable.appendRow();
+			dependenciesTable.setValue(CHARACTERISTIC[1], csticName);
+			String dependencyName = nameProvider.getName(dependency);
+			dependenciesTable.setValue(DEPENDENCY, dependencyName);
+		}
+		execute(function, monitor, "Persisting dependencies for cstic " + csticName);
+		return function;
+	}
 	
 	/**
 	 * Reads global available dependencies.
@@ -67,7 +123,7 @@ public class JCoFunctionPerformer extends BAPIUtils {
 		JCoFunction function = getJCoFunction("CARD_CHAR_READ_ALLOC", monitor);
 		JCoParameterList ipl = function.getImportParameterList();
 		
-		ipl.setValue(JCoFunctionPerformer.CHARACTERISTIC, csticName);
+		ipl.setValue(CHARACTERISTIC[0], csticName);
 		ipl.setValue("LIST_ALL_GLOBL", JCoFunctionPerformer.SELECTED);
 		ipl.setValue("LIST_ALL_LOCAL", JCoFunctionPerformer.SELECTED);
 				
@@ -82,7 +138,7 @@ public class JCoFunctionPerformer extends BAPIUtils {
 	public JCoFunction CARD_CHAR_VAL_READ_ALLOC(String csticName, String valueName, IProgressMonitor monitor, List<Option> objectOptions, List<Option> modelOptions) throws JCoException {
 		JCoFunction function = getJCoFunction("CARD_CHAR_VAL_READ_ALLOC", monitor);
 		JCoParameterList ipl = function.getImportParameterList();
-		ipl.setValue(JCoFunctionPerformer.CHARACTERISTIC, csticName);
+		ipl.setValue(CHARACTERISTIC[0], csticName);
 		ipl.setValue("LIST_ALL_GLOBL", JCoFunctionPerformer.SELECTED);
 		ipl.setValue("LIST_ALL_LOCAL", JCoFunctionPerformer.SELECTED);
 		ipl.setValue("VALUE", valueName);
