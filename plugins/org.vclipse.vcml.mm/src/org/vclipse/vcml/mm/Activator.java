bbc868efd73a90da1cdd76d0398a4162e2c2fc77
AS <as@webxcerpt.com>
2013-02-11 14:15:59 +0000

    changed class path settings, build settings, added activator

---
+++ plugins/org.vclipse.vcml.mm/src/org/vclipse/vcml/mm/Activator.java
@@ -0,0 +1,25 @@
+package org.vclipse.vcml.mm;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+public class Activator extends Plugin {
+
+	public static final String PLUGIN_ID = "org.vclipse.vcml.mm"; //$NON-NLS-1$
+
+	private static Activator plugin;
+	
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	public static Activator getDefault() {
+		return plugin;
+	}
+}
