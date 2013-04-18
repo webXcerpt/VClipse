64e1b0065c9e9b8f0968d86f1bb5d4974481aeb2
Tim Geisler <tg@webxcerpt.com>
2013-04-18 14:01:28 +0000

    fixed extraction of cross referenced objects in source code - cross references have to be extracted in all cases

--- plugins/org.vclipse.vcml.ui.actions/src/org/vclipse/bapi/actions/SourceCrossReferenceExtractor.java
+++ plugins/org.vclipse.vcml.ui.actions/src/org/vclipse/bapi/actions/SourceCrossReferenceExtractor.java
@@ -27,10 +27,7 @@ import org.vclipse.bapi.actions.characteristic.CharacteristicReader;
 import org.vclipse.bapi.actions.classes.ClassReader;
 import org.vclipse.bapi.actions.variantfunction.VariantFunctionReader;
 import org.vclipse.bapi.actions.varianttable.VariantTableReader;
-import org.vclipse.vcml.vcml.Function;
 import org.vclipse.vcml.vcml.Option;
-import org.vclipse.vcml.vcml.PFunction;
-import org.vclipse.vcml.vcml.Table;
 import org.vclipse.vcml.vcml.VCObject;
 import org.vclipse.vcml.vcml.VcmlModel;
 import org.vclipse.vcml.vcml.VcmlPackage;
@@ -56,24 +53,23 @@ public class SourceCrossReferenceExtractor {
 		TreeIterator<EObject> iterator = source.eAllContents();
 		while (iterator.hasNext()) {
 			EObject eObject = iterator.next();
-			if (eObject instanceof Table || eObject instanceof Function || eObject instanceof PFunction) {
-				ICompositeNode node = NodeModelUtils.getNode(eObject);
-				Iterator<INode> nodeIterator = node.getChildren().iterator();
-				while(nodeIterator.hasNext()) {
-					INode childNode = nodeIterator.next();
-					if (childNode.getGrammarElement() instanceof CrossReference) {
-						CrossReference ref = (CrossReference) childNode.getGrammarElement();
-						String name = NodeModelUtils.getTokenText(childNode);
-						EClassifier classifier = ref.getType().getClassifier();
-						if (VcmlPackage.Literals.CHARACTERISTIC == classifier) {
-							csticReader.read(name, model, monitor, seenObjects, options, true);
-						} else if (VcmlPackage.Literals.CLASS == classifier) {
-							classReader.read(name, model, monitor, seenObjects, options, true);
-						} else if (VcmlPackage.Literals.VARIANT_FUNCTION == classifier) {
-							variantFunctionReader.read(name, model, monitor, seenObjects, options, true);
-						} else if (VcmlPackage.Literals.VARIANT_TABLE == classifier) {
-							variantTableReader.read(name, model, monitor, seenObjects, options, true);
-						}
+			ICompositeNode node = NodeModelUtils.getNode(eObject);
+			Iterator<INode> nodeIterator = node.getChildren().iterator();
+			while(nodeIterator.hasNext()) {
+				INode childNode = nodeIterator.next();
+				EObject grammarElement = childNode.getGrammarElement();
+				if (grammarElement instanceof CrossReference) {
+					CrossReference ref = (CrossReference) grammarElement;
+					String name = NodeModelUtils.getTokenText(childNode);
+					EClassifier classifier = ref.getType().getClassifier();
+					if (VcmlPackage.Literals.CHARACTERISTIC == classifier) {
+						csticReader.read(name, model, monitor, seenObjects, options, true);
+					} else if (VcmlPackage.Literals.CLASS == classifier) {
+						classReader.read(name, model, monitor, seenObjects, options, true);
+					} else if (VcmlPackage.Literals.VARIANT_FUNCTION == classifier) {
+						variantFunctionReader.read(name, model, monitor, seenObjects, options, true);
+					} else if (VcmlPackage.Literals.VARIANT_TABLE == classifier) {
+						variantTableReader.read(name, model, monitor, seenObjects, options, true);
 					}
 				}
 			}
